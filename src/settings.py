a02b7f4ffe9ba0c7b22dfe193eff1cd71fcdf8a7
LiuDongChen <ldc31415926@iCloud.com>
2021-06-13 03:42:45 +0800

    更新主screen设置

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,10 @@
+
+
 class Settings:
     def __init__(self):
-        pass
+        self.screen_title = "Screen"
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.screen_bg_color = "#808080"
+
+
