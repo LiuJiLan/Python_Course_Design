f35cb0401d3636080ba3f460ffc08f3651178a89
LiuDongChen <ldc31415926@iCloud.com>
2021-06-15 00:00:49 +0800

    db_func并入data_package
    将db_functions的函数内容并入data_package

--- src/data_package.py
+++ src/data_package.py
@@ -1,4 +1,6 @@
 class Package:
     def __init__(self):
         self.id = ""  # 学工号
-        self.identity = ""  # 选择的身份
+        self.authority = ""  # 选择的身份, 原为identity, 为放置与id弄混而重命名
+        self.user_name = ""
+        self.password = ""
